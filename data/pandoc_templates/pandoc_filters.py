00827cad48756271fe12d367deeec5c72e0d44b0
Phil Ewels <phil.ewels@scilifelab.se>
2014-12-04 15:57:25 +0100

    Removing commented out code

--- data/pandoc_templates/pandoc_filters.py
+++ data/pandoc_templates/pandoc_filters.py
@@ -10,8 +10,7 @@ at the (very very long) JSON output.
 from pandocfilters import toJSONFilter, Image, Str, RawInline
 
 
-# Replace PNG images with PDF
-# NO NEED
+# Replace [tick] or [cross] with different icons in PDF and HTML
 def tick_cross_images (key, value, format, meta):
     template_dir = '/Users/philewels/Work/ngi_reports/data/pandoc_templates/assets/'
     if key == 'Str' and value == '[tick]':
@@ -19,14 +18,12 @@ def tick_cross_images (key, value, format, meta):
             return RawInline('latex', r'~\tickmark~~')
         else:
             return RawInline('html', r'<span class="icon_tick">&#10004;</span> ')
-            # return Image([Str('tick')], ['{}tick.png'.format(template_dir),''])
     
     if key == 'Str' and value == '[cross]':
         if format == "latex":
             return RawInline('latex', r'~\crossmark~~~')
         else:
             return RawInline('html', r'<span class="icon_cross">&#10008;</span> ')
-            # return Image([Str('cross')], ['{}cross.png'.format(template_dir),''])
 
 
 if __name__ == "__main__":
