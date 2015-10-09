266a916e6866f4c79fc904a035fde92705d5d80a
senthil10 <senthilkumar.panneerselvam@scilifelab.se>
2015-10-09 17:20:33 +0200

    adding a dir for utils

---
+++ ngi_reports/utils/config.py
@@ -0,0 +1,21 @@
+""" Load and parse configuration file
+"""
+import ConfigParser
+import os
+
+def load_config(config_file=None):
+    """Loads a configuration file.
+
+    By default it assumes ~/.ngi_config/ngi_reports.conf
+    """
+    try:
+        if not config_file:
+            config_file = os.path.join(os.environ.get('HOME'), '.ngi_config', 'ngi_reports.conf')
+        config = ConfigParser.SafeConfigParser({'ngi_node': 'unknown'})
+        with open(config_file) as f:
+            config.readfp(f)
+        return config
+    except IOError:
+        raise IOError(("There was a problem loading the configuration file. "
+                "Please make sure that ~/.ngi_config/ngi_reports.conf exists "
+                "and that you have read permissions"))
