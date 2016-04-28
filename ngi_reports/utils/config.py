340457a17b946a7870308b8bb7dfa7ca1a42773a
senthil10 <senthilkumar.panneerselvam@scilifelab.se>
2016-04-28 11:44:20 +0200

    Config file fix for IRMA and closes #78

--- ngi_reports/utils/config.py
+++ ngi_reports/utils/config.py
@@ -12,6 +12,8 @@ def load_config(config_file=None):
     try:
         if not config_file:
             config_file = os.path.join(os.environ.get('HOME'), '.ngi_config', 'ngi_reports.conf')
+            if not os.path.exists(config_file):
+                config_file = os.path.join(os.environ.get("NGI_REPORTS_CONFIG"))
         config = ConfigParser.SafeConfigParser({'ngi_node': 'unknown'})
         with open(config_file) as f:
             config.readfp(f)
@@ -19,7 +21,8 @@ def load_config(config_file=None):
     except IOError:
         raise IOError(("There was a problem loading the configuration file. "
                 "Please make sure that ~/.ngi_config/ngi_reports.conf exists "
-                "and that you have read permissions"))
+                "or env vairable 'NGI_REPORTS_CONFIG' is set with path to conf "
+                "file and set with read permissions"))
 
 def expand_path(input_path, substitutions):
     """Use python's string templates to replace substitution patterns in
